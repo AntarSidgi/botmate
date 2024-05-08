c3d04b866e0174c94a00d0137f26677231e9bb24
mxvsh <hi@monawwar.io>
2024-05-08 17:27:10 +0530

    chore: add about page

--- app/[locale]/bots/[botId]/settings/about/page.tsx
+++ app/[locale]/bots/[botId]/settings/about/page.tsx
@@ -1,9 +1,54 @@
+import {
+  Card,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from '#/components/ui/card';
+
 import Header from '#/components/common/header';
 
 function Page() {
   return (
     <>
       <Header title="About" />
+
+      <div className="container space-y-4 py-4">
+        <Card>
+          <CardHeader>
+            <CardTitle className="text-md">
+              BotMate
+            </CardTitle>
+            <CardDescription>
+              Bot development platform for
+              Telegram. Add your own bot and
+              manage it easily.
+            </CardDescription>
+          </CardHeader>
+        </Card>
+
+        <div className="grid grid-cols-2 gap-4">
+          <Card>
+            <CardHeader>
+              <CardTitle className="text-md">
+                v{process.env.version}
+              </CardTitle>
+              <CardDescription>
+                version
+              </CardDescription>
+            </CardHeader>
+          </Card>
+          <Card>
+            <CardHeader>
+              <CardTitle className="text-md">
+                {process.env.gitLastCommit}
+              </CardTitle>
+              <CardDescription>
+                git commit
+              </CardDescription>
+            </CardHeader>
+          </Card>
+        </div>
+      </div>
     </>
   );
 }
--- next.config.mjs
+++ next.config.mjs
@@ -1,10 +1,29 @@
+import { execSync } from 'child_process';
+import { readFileSync } from 'fs';
+
 import createNextIntlPlugin from 'next-intl/plugin';
 
+const packageJSON = JSON.parse(
+  readFileSync('./package.json', {
+    encoding: 'utf-8',
+  }),
+);
+
 const withNextIntl = createNextIntlPlugin();
+
+const revision = execSync(
+  'git rev-parse --short HEAD',
+)
+  .toString()
+  .trim();
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: false,
+  env: {
+    version: packageJSON.version,
+    gitLastCommit: revision,
+  },
 };
 
 export default withNextIntl(nextConfig);
