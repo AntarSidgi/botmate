eede97e2551f2669b19520725c5a617cb86afe78
mxvsh <hi@monawwar.io>
2024-05-09 23:45:28 +0530

    fix: api address

--- instrumentation.ts
+++ instrumentation.ts
@@ -1,3 +1,9 @@
 export function register() {
-  fetch('http://localhost:3000/api/init');
+  const address =
+    process.env.NODE_ENV === 'development'
+      ? 'http://localhost:3000'
+      : process.env.VERCEL_URL ||
+        process.env.APP_URL;
+
+  fetch(`${address}/api/init`);
 }
