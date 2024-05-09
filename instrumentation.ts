48f495a5c71128c12574a31adbd8120eb65ae001
mxvsh <hi@monawwar.io>
2024-05-09 23:49:19 +0530

    fix: include `https`

--- instrumentation.ts
+++ instrumentation.ts
@@ -2,8 +2,8 @@ export function register() {
   const address =
     process.env.NODE_ENV === 'development'
       ? 'http://localhost:3000'
-      : process.env.VERCEL_URL ||
-        process.env.APP_URL;
+      : process.env.APP_URL ||
+        `https://${process.env.VERCEL_URL}`;
 
   fetch(`${address}/api/init`);
 }
