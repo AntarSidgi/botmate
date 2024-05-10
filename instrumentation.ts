106e0257920f8ec33285c59a17485f5b1acc090f
mxvsh <hi@monawwar.io>
2024-05-10 22:36:48 +0530

    chore: add try-catch

--- instrumentation.ts
+++ instrumentation.ts
@@ -1,12 +1,14 @@
 import { isHttpUri, isHttpsUri } from 'valid-url';
 
-export function register() {
+export async function register() {
   const address =
     process.env.NODE_ENV === 'development'
       ? 'http://localhost:3000'
       : process.env.APP_URL ||
         `https://${process.env.VERCEL_URL}`;
 
-  if (isHttpsUri(address) || isHttpUri(address))
-    fetch(`${address}/api/init`);
+  try {
+    if (isHttpsUri(address) || isHttpUri(address))
+      await fetch(`${address}/api/init`);
+  } catch {}
 }
